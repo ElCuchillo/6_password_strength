d9682f0c071c09ef312e9518cb48d8f76b02021a
Liza-liza <lizz4mail@gmail.com>
2017-10-25 16:47:42 +0300

    исправлен шаблон даты и магические числа

--- password_strength.py
+++ password_strength.py
@@ -2,24 +2,29 @@ import re
 
 
 def get_password_strength(password):
-    password_black_list = ['password', 'qwerty', '12345', '111', '555', '777',
+
+    password_blacklist = ['password', 'qwerty', '12345', '111', '555', '777',
                            'test', 'admin', 'god']
+    medium_length = 6
+    good_length = 10
 
-    password_length = (len(password) > 1) * 0 + (len(password) > 6) * 2 + \
-                      (len(password) > 10) * 4
+    password_length = (len(password) > medium_length) * 2 + \
+                      (len(password) > good_length) * 4
 
-    password_has_digit = (re.search('[0-9]', password) != None)
+    password_has_digit = bool(re.search('[0-9]', password))
 
-    password_has_upper = (re.search('[A-ZА-Я]', password) != None)
+    password_has_upper = bool (re.search('[A-ZА-Я]', password))
 
-    password_has_lower = (re.search('[a-zа-я]', password) != None)
+    password_has_lower = bool(re.search('[a-zа-я]', password))
 
-    password_has_special = (re.search('[~!@#$%^&*()_]', password) != None)
+    password_has_special = bool(re.search('[~!@#$%^&*()_]', password))
 
-    password_has_date = (re.search(r'\d{2}-\d{2}-\d{4}', password) != None)
+    password_has_date = bool(re.search(r'''(0[1-9]|[12][0-9]|3[01])[- /.]
+                                       (0[1-9]|1[012])[- /.]\d{2,4}''',
+                                       password))
 
-    password_has_bad_word = (sum(1 for word in password_black_list
-                                 if re.search(word, password.lower())) > 0)
+    password_has_bad_word = bool(sum(1 for word in password_blacklist
+                                 if re.search(word, password.lower())))
 
     password_strength = (password_length + password_has_digit * 1 +
                          password_has_upper * 1 +
@@ -35,6 +40,6 @@ def get_password_strength(password):
 
 if __name__ == '__main__':
     checked_password = input('Your password: ')
-    print('The strength of your password by 10-points scale '
-          '(1 - weak password, 10 - good password) is : ',
+    print('''The strength of your password by 10-points scale 
+          (1 - weak password, 10 - good password) is : ''',
           get_password_strength(checked_password))
